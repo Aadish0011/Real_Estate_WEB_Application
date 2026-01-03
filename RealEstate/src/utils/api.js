28c52a5bab20013fffb540a69c0c6a538a0fc019
Aadish Parashar <104090317+Aadish0011@users.noreply.github.com>
2026-01-04 01:19:25 +0530

    Update api.js

--- RealEstate/src/utils/api.js
+++ RealEstate/src/utils/api.js
@@ -3,7 +3,12 @@
  */
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "http://localhost:8000"
+
 
 // Create axios instance
 const api = axios.create({
