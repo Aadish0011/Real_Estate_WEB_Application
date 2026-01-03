6e790d109b4b7492a80b1b318011f79853c45684
Aadish Parashar <104090317+Aadish0011@users.noreply.github.com>
2026-01-04 00:04:13 +0530

    Modify FastAPI server script for environment config
    Updated the script to use an environment variable for the port and changed the host to '0.0.0.0'.

--- RealEstate/backend/run.py
+++ RealEstate/backend/run.py
@@ -1,14 +1,18 @@
 """
 Script to run the FastAPI server
 """
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+
     uvicorn.run(
         "main:app",
-        host="localhost",
+        host="0.0.0.0",
         port=8000,
         reload=True,
         log_level="info"
     )
+
 
